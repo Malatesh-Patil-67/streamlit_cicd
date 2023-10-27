6ed38e8359e07cc481c639d17300c600b2eaa5be
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-10-27 17:13:44 +0200

    checking

--- test_data.py
+++ test_data.py
@@ -1,35 +1,15 @@
-import streamlit as st
+# test_data.py
+import pytest
 from data import app
 
-# Create a Streamlit test object
-st = st.ScriptRunner(app)
-
 def test_stock_data_app():
-    # Test Case 1: Check if the app loads without errors
+    client = app.test_client()
 
-    # Start the Streamlit app
-    with st:
-        app()  # Call Streamlit app
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Stock Dashboard" in response.data
 
-    # Check if the title is displayed
-    assert st.title("📈 Stock Dashboard")
-
-    # Test Case 2: Check if the app updates with user input
-
-    # Start the Streamlit app
-    with st:
-        app()  # Call  Streamlit app
-
-    # Set a specific symbol using the selectbox
-    st.selectbox("Select a stock symbol:", "AAPL")
-
-    # Check if the Close Price metric updates
-    close_price_element = st.metric("Close Price")
-    assert close_price_element is not None  # Check if the element exists
-
-    # Check if the 52-Week High metric updates
-    high_52_week_element = st.metric("52-Week High")
-    assert high_52_week_element is not None  # Check if the element exists
-
-   
-
+    response = client.get("/?symbol=AAPL")
+    assert response.status_code == 200
+    assert b"Close Price" in response.data
+    assert b"52-Week High" in response.data
