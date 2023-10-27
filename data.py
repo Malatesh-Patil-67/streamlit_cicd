6e544cf862ecacd37b4de540f3b152cc034b5585
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-10-27 16:54:50 +0200

    hu

--- data.py
+++ data.py
@@ -100,8 +100,7 @@ def app():
                 high=stock_data['High'],
                 low=stock_data['Low'],
                 close=stock_data['Close']
-            )
-        ]) 
+            )]) 
     candlestick_chart.update_layout(title=f"{symbol} Candlestick Chart",
                                     xaxis_rangeslider_visible=False)
     st.plotly_chart(candlestick_chart, use_container_width=True)
