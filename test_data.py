95a4b98267dd858b98b28a76c8d8190874e25610
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-10-30 14:25:38 +0100

    pytest 1st try in pipeline

--- test_data.py
+++ test_data.py
@@ -1,8 +1,19 @@
 import re
 import requests
+import pandas as pd
+from data import calculate_price_difference
 
-def test_stock_data_app():
+
+#def test_stock_data_app():
     # Test Case 1: Check if the app loads without errors
-    response = requests.get("http://localhost:8501")
-    assert response.status_code == 200
-    assert re.search(r'Stock Dashboard', response.text) is not None
+    #response = requests.get("http://localhost:8501")
+    #assert response.status_code == 200
+    #assert re.search(r'Stock Dashboard', response.text) is not None
+
+def test_calculate_price_difference():
+    #Test case 2: check Price_difference
+    data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
+                         'Close': [100.0, 110.0]})
+    price_diff, percentage_diff = calculate_price_difference(data)
+    assert price_diff == 10.0
+    assert percentage_diff == 10.0
