20d6e87d95207774454a3521135fc6af3f5ad25f
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-10-31 10:28:05 +0100

    removed a 1st test case

--- test_data.py
+++ test_data.py
@@ -4,11 +4,11 @@ import pandas as pd
 from data import calculate_price_difference
 from data import get_stock_data
 
-def test_stock_data_app():
+#def test_stock_data_app():
      #Test Case 1: Check if the app loads without errors
-    response = requests.get("http://localhost:8501")
-    assert response.status_code == 200
-    assert re.search(r'Stock Dashboard', response.text) is not None
+    #response = requests.get("http://localhost:8501")
+    #assert response.status_code == 200
+    #assert re.search(r'Stock Dashboard', response.text) is not None
 
 def test_calculate_price_difference():
     #Test case 2: check Price_difference
