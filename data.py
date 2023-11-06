82ce6dcf27d13da7a8b8dd4574cd6a71884ae815
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-11-06 13:53:41 +0100

    here we go

--- data.py
+++ data.py
@@ -79,18 +79,29 @@ def app():
             max_52_week_high = stock_data["High"].tail(252).max()
             min_52_week_low = stock_data["Low"].tail(252).min()
         else:
-            price_difference, percentage_difference, latest_close_price, max_52_week_high, min_52_week_low = None, None, None, None, None
-
+            price_difference, percentage_difference, latest_close_price, max_52_week_high, min_52_week_low = (
+                 None, None, None, None, None
+            )
         col1, col2, col3, col4 = st.columns(4)
         with col1:
-            st.metric("Close Price", f"${latest_close_price:.2f}" if latest_close_price is not None else "N/A")
+           st.metric(
+             "Close Price",
+                f"${latest_close_price:.2f}" if latest_close_price is not None else "N/A"
+                    )
         with col2:
-            st.metric("Price Difference (YoY)", f"${price_difference:.2f}" if price_difference is not None else "N/A",
-                    f"{percentage_difference:+.2f}%" if percentage_difference is not None else "N/A")
+            st.metric(
+                "Price Difference (YoY)", f"${price_difference:.2f}"
+                    if price_difference is not None else "N/A",
+                     f"{percentage_difference:+.2f}%" if percentage_difference is not None else "N/A"
+                     )
         with col3:
-            st.metric("52-Week High", f"${max_52_week_high:.2f}" if max_52_week_high is not None else "N/A")
+            st.metric(
+                "52-Week High", f"${max_52_week_high:.2f}" if max_52_week_high is not None else "N/A"
+                )
         with col4:
-            st.metric("52-Week Low", f"${min_52_week_low:.2f}" if min_52_week_low is not None else "N/A")
+            st.metric(
+                "52-Week Low", f"${min_52_week_low:.2f}" if min_52_week_low is not None else "N/A"
+                )
 
     st.subheader("Candlestick Chart")
     if stock_data is not None:
