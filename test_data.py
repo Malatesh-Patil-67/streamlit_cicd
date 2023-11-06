03acfe0d71c22598e5407c6bafde9fb7fc74fe44
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-11-06 15:17:06 +0100

    pytest in pipeline

--- test_data.py
+++ test_data.py
@@ -4,18 +4,17 @@ import pandas as pd
 from data import calculate_price_difference
 from data import get_stock_data
 
-#def test_stock_data_app():
-     #Test Case 1: Check if the app loads without errors
-    #response = requests.get("http://localhost:8501")
-    #assert response.status_code == 200
-    #assert re.search(r'Stock Dashboard', response.text) is not None
+
+
 
 def test_calculate_price_difference():
-    # Test case 1: Check price difference for valid data
-    data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
-                         'Close': [100.0, 110.0]})
-    price_diff, percentage_diff = calculate_price_difference(data)
-    assert (price_diff == 10.0) if price_diff is not None else None
+        # Test case: Check price difference for valid data
+        data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
+                            'Close': [100.0, 110.0]})
+        price_diff, percentage_diff = calculate_price_difference(data)
+        assert price_diff == 10.0
+        assert percentage_diff == 10.0
+
 
 
 
