30273fe7c0533a5dd33fb70bc74b6b0f796fc32e
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-11-07 08:35:10 +0100

    added artifacts

--- test_data.py
+++ test_data.py
@@ -4,11 +4,8 @@ import pandas as pd
 from data import calculate_price_difference
 from data import get_stock_data
 
-
-
-
 def test_calculate_price_difference():
-        # Test case: Check price difference for valid data
+        # Test case: Check price difference for a valid data
         data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
                             'Close': [100.0, 110.0]})
         price_diff, percentage_diff = calculate_price_difference(data)
