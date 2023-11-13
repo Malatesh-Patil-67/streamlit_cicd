e888763d4a5542c5e2694400110025a4bc7862c5
Malatesh-Patil-67 <malateshpatil6767@gmail.com>
2023-11-13 09:34:24 +0100

    Nochmal

--- test_data.py
+++ test_data.py
@@ -1,19 +1,24 @@
-import re
-import requests
+"""
+This module provides test functions 
+"""
 import pandas as pd
 from data import calculate_price_difference
 from data import get_stock_data
 
 def test_calculate_price_difference():
-        # Test case: Check price difference for a valid data
-        data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
+    """    
+    Test case: Check price difference for a valid data
+    """
+    data = pd.DataFrame({'date': ['2022-01-01', '2023-01-01'],
                             'Close': [100.0, 110.0]})
-        price_diff, percentage_diff = calculate_price_difference(data)
-        assert price_diff == 10.0
-        assert percentage_diff == 10.0
+    price_diff, percentage_diff = calculate_price_difference(data)
+    assert price_diff == 10.0
+    assert percentage_diff == 10.0
 
 def test_get_stock_data():
-    # Test case 1: Check if valid data is returned
+    """    
+    Test case 1: Check if valid data is returned
+    """
     symbol = "AAPL"
     data = get_stock_data(symbol)
     assert data is not None
@@ -23,9 +28,3 @@ def test_get_stock_data():
     symbol = "XYZ"
     data = get_stock_data(symbol)
     assert data is None
-
-
-
-
-
-
